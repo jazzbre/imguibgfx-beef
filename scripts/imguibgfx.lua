526e585ed0304e20816df13274e700ca9448f6c0
Aleš Mlakar <ales.ambits@gmail.com>
2020-12-30 11:07:10 +0100

    NEW: Working linux

--- scripts/imguibgfx.lua
+++ scripts/imguibgfx.lua
@@ -1,30 +1,37 @@
 project "imguibgfx"
 	kind "StaticLib"
-   windowstargetplatformversion("10.0")
+	windowstargetplatformversion("10.0")
 
-   includedirs {
-      path.join(SOURCE_DIR, "cimgui/imgui"),
-      path.join(SOURCE_DIR, "bx/include"),
-      path.join(SOURCE_DIR, "../../sdl2-beef/submodules/SDL/include"),
-      path.join(SOURCE_DIR, "cimgui/imgui/examples"),
-      path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/include"),
-      path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/examples/common/imgui"),
-   }
+	includedirs {
+		path.join(SOURCE_DIR, "cimgui/imgui"),
+		path.join(SOURCE_DIR, "bx/include"),
+		path.join(SOURCE_DIR, "../../sdl2-beef/submodules/SDL/include"),
+		path.join(SOURCE_DIR, "cimgui/imgui/examples"),
+		path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/include"),
+		path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/examples/common/imgui"),
+	}
 
-   defines {
-   }
+	defines {
+	}
 
 	files {
-      path.join(SOURCE_DIR, "cimgui/cimgui.c*"),
-      path.join(SOURCE_DIR, "cimgui/cimgui.h*"),
-      path.join(SOURCE_DIR, "cimgui/imgui/*.c*"),
-      path.join(SOURCE_DIR, "cimgui/imgui/*.h*"),
-      path.join(SOURCE_DIR, "cimgui/imgui/*.h*"),
-      -- SDL2
-      path.join(SOURCE_DIR, "cimgui/imgui/examples/imgui_impl_sdl.*"),
-      -- bgfx
-      path.join(SOURCE_DIR, "../csrc/imguibgfx.cc"),
-      path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/examples/common/imgui/*.h"),
+		path.join(SOURCE_DIR, "cimgui/cimgui.c*"),
+		path.join(SOURCE_DIR, "cimgui/cimgui.h*"),
+		path.join(SOURCE_DIR, "cimgui/imgui/*.c*"),
+		path.join(SOURCE_DIR, "cimgui/imgui/*.h*"),
+		path.join(SOURCE_DIR, "cimgui/imgui/*.h*"),
+		-- SDL2
+		path.join(SOURCE_DIR, "cimgui/imgui/examples/imgui_impl_sdl.*"),
+		-- bgfx
+		path.join(SOURCE_DIR, "../csrc/imguibgfx.cc"),
+		path.join(SOURCE_DIR, "../../bgfx-beef/submodules/bgfx/examples/common/imgui/*.h"),
 	}
 
 	configuration {}
+	
+	configuration { "linux*" }
+	defines {
+		"SDL_VIDEO_DRIVER_X11"
+	}
+	
+	configuration {}	
