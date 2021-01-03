e5cb4be4bc47ef2801a21ba01cc950678685c240
Aleš Mlakar <ales.ambits@gmail.com>
2021-01-03 11:28:52 +0100

    UPDATE: Latest submodules + macOS x64/arm64 split

--- build_macos.sh
+++ build_macos.sh
@@ -1,3 +1,3 @@
-./submodules/bx/tools/bin/darwin/genie --gcc=osx gmake
-make -C .build/projects/gmake-osx config=debug64 -j 8
-make -C .build/projects/gmake-osx config=release64 -j 8
+./submodules/bx/tools/bin/darwin/genie --gcc=osx-x64 gmake
+make -C .build/projects/gmake-osx-x64 config=debug -j 8
+make -C .build/projects/gmake-osx-x64 config=release -j 8
